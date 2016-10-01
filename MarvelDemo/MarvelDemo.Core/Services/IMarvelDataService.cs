857a02737a35fb4818c268cc8570dad0c07ef8e5
Matt Wilhelm <github@addressxception.com>
2016-10-01 12:22:03 -0400

    Adjust spacing

--- MarvelDemo/MarvelDemo.Core/Services/IMarvelDataService.cs
+++ MarvelDemo/MarvelDemo.Core/Services/IMarvelDataService.cs
@@ -6,6 +6,6 @@ namespace MarvelDemo.Core.Services
 {
     public interface IMarvelDataService
     {
-        Task<IEnumerable<Comic>> GetComicsBySeries(int seriesId);
+        Task<IEnumerable<Comic>>GetComicsBySeries(int seriesId);
     }
 }
