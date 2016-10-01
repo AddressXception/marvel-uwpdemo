004cc0b1fe94a2b05cc5e6b63fd6409484e038a8
Matt Wilhelm <github@addressxception.com>
2016-10-01 15:03:02 -0400

    add iron man to the scene

---
+++ Marvel-Unity/Assets/Scripts/PickCharacter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickCharacter : MonoBehaviour
+{
+
+    private GameObject SuperHero;
+
+    public GameObject IronManPrefab;
+
+    public GameObject CaptainPrefab;
+
+    public GameObject ThorPrefab;
+
+    public Camera MainCamera { get { return GetComponent<Camera>(); } }
+
+	// Use this for initialization
+	void Start ()
+	{
+	    SuperHero = IronManPrefab;
+	    Instantiate(SuperHero);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
