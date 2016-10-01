e87e4c33d49b5e1ce9c858a32622017f295c7402
Matt Wilhelm <github@addressxception.com>
2016-10-01 11:45:23 -0400

    use larger icons

--- MarvelDemo/MarvelDemo.Core/ViewModels/MainShellViewModel.cs
+++ MarvelDemo/MarvelDemo.Core/ViewModels/MainShellViewModel.cs
@@ -20,9 +20,9 @@ namespace MarvelDemo.Core.ViewModels
         {
             NavItems = new ObservableCollection<Character>();
 
-            NavItems.Add(new Character {Id = 1009368, Name = "Iron Man", IconPath = "../Assets/NavIcons/ironman_52.png", SeriesId = 2029});
-            NavItems.Add(new Character { Id = 1009220, Name = "Captain America", IconPath = "../Assets/NavIcons/captainamerica_52.png", SeriesId = 1996 });
-            NavItems.Add(new Character { Id = 1009664, Name = "Thor", IconPath = "../Assets/NavIcons/thor_52.png", SeriesId = 2083 });
+            NavItems.Add(new Character {Id = 1009368, Name = "Iron Man", IconPath = "../Assets/NavIcons/ironman_100.png", SeriesId = 2029});
+            NavItems.Add(new Character { Id = 1009220, Name = "Captain America", IconPath = "../Assets/NavIcons/captainamerica_100.png", SeriesId = 1996 });
+            NavItems.Add(new Character { Id = 1009664, Name = "Thor", IconPath = "../Assets/NavIcons/thor_100.png", SeriesId = 2083 });
         }
     }
 }
